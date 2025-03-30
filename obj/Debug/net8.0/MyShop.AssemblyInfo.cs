61f69da2ebbf89647449848b5384fae18999de2b
jackson-muema <muthianjackson1@gmail.com>
2025-03-30 13:17:16 +0300

    Quick fixes

--- obj/Debug/net8.0/MyShop.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyShop.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2f2de2e012f7cf9875d7a4d1e765b18100d0dbc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94ad6a40441eafd0f888e0b688be5193f3786da7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
